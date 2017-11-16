8b7953afc7b298650bbbdf1a3fc0c1889444397c
svaduka <sainagaraju.vaduka@gmail.com>
2017-11-16 07:50:40 +0530

    Created BATCH161017 Project Directory

---
+++ BATCH161017/src/com/laboros/hdfs/HDFSServicePut.java
@@ -0,0 +1,19 @@
+package com.laboros.hdfs;
+
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.util.Tool;
+
+public class HDFSServicePut extends Configured implements Tool {
+
+	public static void main(String[] args) {
+	
+	}
+	
+	@Override
+	public int run(String[] args) throws Exception {
+	
+		return 0;
+	
+	}
+
+}
