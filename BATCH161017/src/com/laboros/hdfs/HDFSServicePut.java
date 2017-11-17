84595f2f6f71fa28fe92e186b79ad6f7a1de4edc
svaduka <sainagaraju.vaduka@gmail.com>
2017-11-17 08:44:21 +0530

    Completed HDFSService Code  java -cp ../HadoopJars/*:HDFSServicePutDemo.jar com.laboros.hdfs.HDFSServicePut WordCount.txt /user/trainings

--- BATCH161017/src/com/laboros/hdfs/HDFSServicePut.java
+++ BATCH161017/src/com/laboros/hdfs/HDFSServicePut.java
@@ -1,9 +1,14 @@
 package com.laboros.hdfs;
 
-import java.util.Map;
+import java.io.FileInputStream;
+import java.io.InputStream;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IOUtils;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
@@ -20,6 +25,8 @@ public class HDFSServicePut extends Configured implements Tool {
 		}
 		// Loading Configuration
 		Configuration conf = new Configuration(Boolean.TRUE);
+		conf.set("fs.defaultFS", "hdfs://localhost:8020");
+		
 //		print(conf);
 		
 		// Invoke ToolRunner.run
@@ -39,6 +46,47 @@ public class HDFSServicePut extends Configured implements Tool {
 	@Override
 	public int run(String[] args) throws Exception {
 		
+		//Creating a file on hdfs = creating metadata + Add data
+		
+		/**
+		 * Step:1 : Creating metadata = Creating empty file 
+		 * on hdfs and put the metadata in the namenode 
+		*/ 
+		
+		//Get the edge node file = get the file from argumnet
+		final String edgeNodeInputFile = args[0];
+		System.out.println("Input EdgeNode Local File:"+edgeNodeInputFile);
+		//get the hdfs destination directory == get the information from argument
+		
+		final String hdfsDestinationDir=args[1];
+		System.out.println("HDFS Destination dir:"+hdfsDestinationDir);
+		
+		//Convert into URI because hdfs understand URI
+		final Path hdfsDestPathWithFileName = new Path(hdfsDestinationDir, edgeNodeInputFile);
+		
+		//Create the FileSystem
+		
+		Configuration conf = super.getConf();
+		
+		FileSystem hdfs = FileSystem.get(conf);
+		
+		FSDataOutputStream fsdos=hdfs.create(hdfsDestPathWithFileName);
+		
+		/**Step:2 
+		 * 
+		 * Adding Data
+		 * 
+		 * Splitting Data into blocks
+		 * Identify the Datanode for data block -- DataStreamer
+		 * Writing datablock to DN
+		 * Replication
+		 * Sync with NN
+		 * Handling Failures.
+		 */
+		
+		InputStream is = new FileInputStream(edgeNodeInputFile);
+		
+		IOUtils.copyBytes(is, fsdos, conf, Boolean.TRUE);
 		
 
 		return 0;
