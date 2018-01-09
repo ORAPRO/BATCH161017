149b6e7a887c2a83a230cf76e4acdcb8fa22142a
svaduka <sainagaraju.vaduka@gmail.com>
2018-01-09 08:38:03 +0530

    Weather UseCase

--- BATCH161017/src/com/laboros/hdfs/HDFCServicePut_RAFI.java
+++ BATCH161017/src/com/laboros/hdfs/HDFCServicePut_RAFI.java
@@ -9,7 +9,6 @@ import java.io.InputStream;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.FSDataOutputStream;
-import org.apache.hadoop.fs.FileAlreadyExistsException;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IOUtils;
